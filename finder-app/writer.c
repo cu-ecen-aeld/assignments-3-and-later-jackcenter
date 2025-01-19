cf6faad2ced04c3d73b731d4f7a1abeb5b0108a8
Jack <jack.d.center@gmail.com>
2025-01-19 16:03:14 -0700

    Add Logging

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -2,42 +2,45 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <syslog.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+  openlog("writer", LOG_PID, LOG_USER);
+  syslog(LOG_DEBUG, "Starting `writer`.");
+
   if (argc != 3) {
     printf("%d\r\n", argc);
     printf("Usage: %s [FILE] [TEXT]\r\n", argv[0]);
     return 1;
   }
 
-  // Grab the args
+  // Parse the arguments
   const char* file = argv[1];
   const char* data = argv[2];
 
-  printf("File: %s, Data: %s\r\n", file, data);
-
-  // Try to open
+  // Open the file
   const int file_descriptor = open(file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH );
   if (file_descriptor == -1) {
     const int err = errno;
-    printf("Error: open - %d: %s\r\n", err, strerror(err));
+    syslog(LOG_ERR, "%d: %s", err, strerror(err));
     return -1;
   }
 
   // Write the data
   const size_t data_length = strlen(data);
+  syslog(LOG_DEBUG, "Writing %s to %s.", data, file);
   const ssize_t nr = write(file_descriptor, data, data_length);
   if (nr == -1) {
     const int err = errno;
-    printf("Error: write - %d: %s\r\n", err, strerror(err));
+    syslog(LOG_ERR, "%d: %s", err, strerror(err));
     return -1;
   } else if (nr != data_length) {
-    printf("Error: partial write\r\n");
+    syslog(LOG_ERR, "Error: partial write");
     return -1;
   }
 
-
-  printf("Fin\r\n");
+  syslog(LOG_DEBUG, "Completing `writer`.");
+  closelog();
   return 0;
 }
